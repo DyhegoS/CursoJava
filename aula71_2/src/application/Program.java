bfea767c8449c74cf19fa9af61d10f072f34f0d1
Dyhego Silva <dyhegodasilva@gmail.com>
2025-03-10 11:51:36 -0300

    alterado para a versão três com metodo estatico

--- aula71_2/src/application/Program.java
+++ aula71_2/src/application/Program.java
@@ -11,18 +11,19 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		Calculator calc = new Calculator();
 		
 		System.out.print("Enter radius:");
 		double radius = sc.nextDouble();
 		
-		double c = calc.circumference(radius);
+		double c = Calculator.circumference(radius);
+		//quando o metodo é estatico eu não preciso criar uma variavel que 
+		//instancia a classe, eu chamo a classe direto
 		
-		double v = calc.volume(radius);
+		double v = Calculator.volume(radius);
 		
 		System.out.printf("Circumference: %.2f%n", c);
 		System.out.printf("Valume: %.2f%n", v);
-		System.out.printf("PI: %.2f%n", calc.PI);
+		System.out.printf("PI: %.2f%n", Calculator.PI);
 		
 		sc.close();
 
